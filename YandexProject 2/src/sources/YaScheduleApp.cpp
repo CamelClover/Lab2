d6840af5f1f46aa80e86178440d5e32f1e378054
CamelClover <164196984+CamelClover@users.noreply.github.com>
2024-04-14 19:25:49 +0300

    Add files via upload

---
+++ YandexProject 2/src/sources/YaScheduleApp.cpp
@@ -0,0 +1,81 @@
+#include <YaScheduleApp.h>
+
+
+using json = nlohmann::json;
+
+
+YaScheduleApp::YaScheduleApp(std::string openWeatherApiKey, std::string yaScheduleApiKey,
+	std::string cityFrom, std::string cityTo) {
+	this->openWeatherClient = new httplib::Client("http://api.openweathermap.org");
+	this->yandexScheduleClient = new httplib::Client("http://api.rasp.yandex.net");
+	this->cityFrom = cityFrom;
+	this->cityTo = cityTo;
+	this->openWeatherApiKey = openWeatherApiKey;
+	this->yaScheduleApiKey = yaScheduleApiKey;
+}
+
+
+json YaScheduleApp::makeRequest(httplib::Client* client, std::string request) {
+	if (auto response = client->Get(request)) {
+		if (response->status == 200) {
+			return json::parse(response->body);
+		}
+		else {
+			std::cout << "Bad request " << response->status << std::endl;
+		}
+	}
+	return *new json;
+}
+
+
+std::vector <std::string> YaScheduleApp::getLatLon(std::string city) {
+	std::string request = this->openWeatherRequest;
+	request += "q=" + city;
+	request += "&appid=" + this->openWeatherApiKey;
+	json result = this->makeRequest(this->openWeatherClient, request);
+	std::vector <std::string> latLon = { to_string(result["coord"]["lon"]), to_string(result["coord"]["lat"])};
+	return latLon;
+}
+
+
+std::string YaScheduleApp::getCityCode(std::vector <std::string> latLon) {
+	std::string request = this->yaCityCodeRequest;
+	request += "apikey=" + this->yaScheduleApiKey;
+	request += "&lat=" + latLon[1];
+	request += "&lng=" + latLon[0];
+	std::string code = this->makeRequest(this->yandexScheduleClient, request)["code"].get<std::string>();
+	return code;
+}
+
+
+json YaScheduleApp::getScheduleBetweenCities() {
+	std::string request = this->yaScheduleRequest;
+	request += "apikey=" + this->yaScheduleApiKey;
+	request += "&from=" + this->getCityCode(this->getLatLon(this->cityFrom));
+	request += "&to=" + this->getCityCode(this->getLatLon(this->cityTo));
+	json result = this->makeRequest(this->yandexScheduleClient, request);
+	std::map <std::string, std::string> raceTitle;
+	std::map <std::string, std::string> raceNumber;
+	std::map <std::string, std::string> transportType;
+	std::map <std::string, std::string> transportCompanyCode;
+	std::map <std::string, std::string> vehicleModel;
+	std::map <std::string, std::string> departureTimeAndArrivalTime;
+	std::map <std::string, std::string> AreThereAnyTransfers;
+	std::map <std::string, std::string> flightPrice;
+	for (int i = 0; i < result["segments"].size(); i++){
+		std::vector <std::map <std::string, std::string>> race{};
+		raceTitle["Race Title"] = result["segments"][i]["thread"]["title"].get<std::string>();
+		raceNumber["Race Number"] = result["segments"][i]["thread"]["number"].get<std::string>();
+		transportType["Transport Type"] = result["segments"][i]["from"]["transport_type"].get<std::string>();
+		//что-то должно быть написано
+		race.push_back(raceTitle);
+		race.push_back(raceNumber);
+		race.push_back(transportType);
+		race.push_back(transportCompanyCode);
+		race.push_back(vehicleModel);
+		race.push_back(departureTimeAndArrivalTime);
+		race.push_back(AreThereAnyTransfers);
+		race.push_back(flightPrice);
+		this->schedule.push_back(race);
+	}
+}
